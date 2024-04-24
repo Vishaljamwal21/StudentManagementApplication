9e2ad097881335d4bd6e243c7e26a58a663f271c
Vishal Jamwal <vishaljamwal402@gmail.com>
2024-04-24 11:11:04 +0530

    API-Urls

---
+++ StudentManagementApplication/SMS-APP/URL.cs
@@ -0,0 +1,14 @@
+﻿namespace SMS_APP
+{
+    public class URL
+    {
+        public static string APIBaseURl = "https://localhost:7077/";
+        public static string StudentAPIPath = APIBaseURl + "api/Student";
+        public static string CourseAPIPath = APIBaseURl + "api/Course";
+        public static string EnrollmentAPIPath = APIBaseURl + "api/Enrollment";
+        public static string GradeAPIPath = APIBaseURl + "api/Grade";
+        // UserController URLs
+        public static string AuthenticateAPIPath = APIBaseURl + "api/User/Autheticate";
+        public static string RegisterAPIPath = APIBaseURl + "api/User/Register";
+    }
+}
