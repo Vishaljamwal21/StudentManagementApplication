2074585205a335b416fcd9ba952ac33a18b88f26
Vishal Jamwal <vishaljamwal402@gmail.com>
2024-04-24 15:27:26 +0530

    Course-CRUD

---
+++ StudentManagementApplication/SMS-APP/Controllers/CourseController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMS_APP.Models;
+using SMS_APP.Repository.IRepository;
+
+namespace SMS_APP.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+        public CourseController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;      
+        }
+
+        #region APIs
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _courseRepository.GetAllAsync(URL.CourseAPIPath) });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var course = await _courseRepository.GetAsync(URL.CourseAPIPath, id);
+            if (course == null)
+                return Json(new { success = false, message = "Unable to delete the data" });
+
+            await _courseRepository.DeleteAsync(URL.CourseAPIPath, id);
+            return Json(new { success = true, message = "Data Deleted Successfully" });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SaveOrUpdate(int? id)
+        {
+            Course course = new Course();
+            if (id != null)
+            {
+                course = await _courseRepository.GetAsync(URL.CourseAPIPath, id.Value);
+                if (course == null)
+                    return NotFound();
+            }
+
+            return View(course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveOrUpdate(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                if (course.Id == 0)
+                    await _courseRepository.CreateAsync(URL.CourseAPIPath, course);
+                else
+                    await _courseRepository.UpdateAsync(URL.CourseAPIPath, course);
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(course);
+            }
+        }
+    }
+}
---
+++ StudentManagementApplication/SMS-APP/wwwroot/js/Course.js
@@ -0,0 +1,54 @@
+﻿var datatable;
+$(document).ready(function () {
+    loaddatatable();
+})
+function loaddatatable() {
+    datatable = $("#tabledata").DataTable({
+        "ajax": {
+            "url": "Course/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "title", "width": "20%" },
+            { "data": "description", "width": "20%" },
+            { "data": "duration", "width": "20%" },           
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                           <a class="btn btn-primary" href="/Course/SaveorUpdate/${data}"><i class="fas fa-edit"></i></a>
+                            <a class="btn btn-danger" onclick=Delete("Course/Delete/${data}")><i class="fas fa-trash"></i></a>
+                            </div>
+                            `;
+                }
+            }
+        ]
+    })
+}
+function Delete(url) {
+    swal({
+        text: "Delete Course",
+        title: "Do you want to delete the information",
+        buttons: true,
+        icon: "warning",
+        dangermodel: true
+    }).then((willdelete) => {
+        if (willdelete) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        datatable.ajax.reload();
+                        toastr.success(data.messege);
+                    }
+                    else {
+                        toastr(data.messege);
+                    }
+                }
+            })
+        }
+    })
+}
